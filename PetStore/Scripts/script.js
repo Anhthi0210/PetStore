c5f3e06417d3ce96e89fd08b469cf40dbc6a4196
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-03-12 18:28:12 +0700

    edit menu3

--- PetStore/Scripts/script.js
+++ PetStore/Scripts/script.js
@@ -8,3 +8,10 @@ const switch_mode = document.querySelector('flexSwitchCheckDefault');
 switch_mode.addEventListener('check', () => {
     alert('checked');
 });
+//menu3
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navbarToggler.addEventListener('click', () => {
+    navbarCollapse.classList.toggle('show');
+});
