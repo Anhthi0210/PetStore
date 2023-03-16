beeca4b90a779cffdd7384de347ed7567dc9dfb8
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-03-16 17:36:30 +0700

    doi menu

--- PetStore/Scripts/script.js
+++ PetStore/Scripts/script.js
@@ -1,17 +1,1 @@
-﻿//const form-check - input = document.getElementById('flexSwitchCheckDefault');
-
-//                        form - check - input.addEventListener('change', () => {
-//        document.body.classList.toggle('dark');
-//                        });
-const body = document.body;
-const switch_mode = document.querySelector('flexSwitchCheckDefault');
-switch_mode.addEventListener('check', () => {
-    alert('checked');
-});
-//menu3
-const navbarToggler = document.querySelector('.navbar-toggler');
-const navbarCollapse = document.querySelector('.navbar-collapse');
-
-navbarToggler.addEventListener('click', () => {
-    navbarCollapse.classList.toggle('show');
-});
+﻿
