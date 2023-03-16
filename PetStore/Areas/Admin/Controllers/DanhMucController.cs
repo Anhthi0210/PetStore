bd41c5eed9234bf7d90023e1a4670ee35ae88cea
Louis Phan <imthanq7@outlook.com>
2023-03-16 10:38:50 +0700

    make color

---
+++ PetStore/Areas/Admin/Controllers/DanhMucController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetStore.Models;
+
+namespace PetStore.Areas.Admin.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Admin/DanhMuc
+        public ActionResult Index()
+        {
+            return View(db.DANHMUCs.ToList());
+        }
+
+        // GET: Admin/DanhMuc/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DANHMUC dANHMUC = db.DANHMUCs.Find(id);
+            if (dANHMUC == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dANHMUC);
+        }
+
+        // GET: Admin/DanhMuc/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/DanhMuc/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaDM,TenDM")] DANHMUC dANHMUC)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DANHMUCs.Add(dANHMUC);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dANHMUC);
+        }
+
+        // GET: Admin/DanhMuc/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DANHMUC dANHMUC = db.DANHMUCs.Find(id);
+            if (dANHMUC == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dANHMUC);
+        }
+
+        // POST: Admin/DanhMuc/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaDM,TenDM")] DANHMUC dANHMUC)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dANHMUC).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dANHMUC);
+        }
+
+        // GET: Admin/DanhMuc/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DANHMUC dANHMUC = db.DANHMUCs.Find(id);
+            if (dANHMUC == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dANHMUC);
+        }
+
+        // POST: Admin/DanhMuc/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            DANHMUC dANHMUC = db.DANHMUCs.Find(id);
+            db.DANHMUCs.Remove(dANHMUC);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
