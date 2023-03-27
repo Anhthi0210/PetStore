f1db97717d9668ecf7b35bdd9ced686686f1e5ed
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-03-27 15:10:14 +0700

    dang nhap admin

--- PetStore/Areas/Admin/Controllers/TaiKhoanController.cs
+++ PetStore/Areas/Admin/Controllers/TaiKhoanController.cs
@@ -128,5 +128,48 @@ namespace PetStore.Areas.Admin.Controllers
             }
             base.Dispose(disposing);
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                TAIKHOAN ad = db.TAIKHOAN.SingleOrDefault(n => n.TenDangNhap.Trim() == tendn.Trim() && n.MatKhau.Trim() == matkhau.Trim());
+                if (ad != null)
+                {
+                    ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["TaikhoanAD"] = ad;
+                    Session["UserName"] = ad.TenDangNhap;
+                    Session["PhanQuyen"] = ad.PHANQUYEN.TenPQ;
+
+
+                    return RedirectToAction("Index", "Dashbroad");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+
+            Session["TaikhoanAD"] = "";
+            Session["UserName"] = "";
+
+            return RedirectToAction("Login", "Admin/TaiKhoan");
+        }
     }
 }
