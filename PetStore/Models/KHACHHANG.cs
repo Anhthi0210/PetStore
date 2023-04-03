39cabc794a88fd7ef9e8088b6c3351daff7c4dec
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-04-03 13:12:33 +0700

    v

--- PetStore/Models/KHACHHANG.cs
+++ PetStore/Models/KHACHHANG.cs
@@ -54,7 +54,7 @@ namespace PetStore.Models
         [RegularExpression(@"^[a-zA-Z0-9]+@gmail\.com$", ErrorMessage = "Địa chỉ email không hợp lệ!")]
         public string Email { get; set; }
 
-        //[Required(ErrorMessage = "Cannot be blank!")]
+        [Required(ErrorMessage = "Cannot be blank!")]
         [Display(Name = "Điểm tích lũy")]
         public int? DiemTichLuy { get; set; }
 
@@ -67,7 +67,7 @@ namespace PetStore.Models
         [Required(ErrorMessage = "Cannot be blank!")]
         [Display(Name = "Mật khẩu")]
         [StringLength(50)]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[!@#$%^&*+=])(?=.{8,})\\S+$", ErrorMessage = "Mật khẩu phải có ít nhất 8 kí tự và gồm ít nhất 1 kí tự đặc biệt và 1 chữ in")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=_!])(?!.*\s).{8,}$", ErrorMessage = "Mật khẩu phải có ít nhất 8 kí tự và gồm ít nhất 1 kí tự đặc biệt, 1 chữ in, 1 chữ thường")]
         public string MatKhau { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
