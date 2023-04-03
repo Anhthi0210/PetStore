aff3fb0aa4a87d6f368d8dc9ebed220dce43626d
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-04-03 21:21:30 +0700

    fgd

--- PetStore/Models/DONHANG.cs
+++ PetStore/Models/DONHANG.cs
@@ -19,27 +19,27 @@ namespace PetStore.Models
         [Required(ErrorMessage = "Cannot be blank!")]
         public int MaDH { get; set; }
 
-        [Required(ErrorMessage = "Cannot be blank!")]
+        //[Required(ErrorMessage = "Cannot be blank!")]
         [Display(Name = "Người Nhận")]
         [RegularExpression(@"^[a-zA-Z0-9\sàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹ]+$", ErrorMessage = "Người nhận cannot use special characters!")]
         [StringLength(50)]
         public string TenNguoiNhan { get; set; }
 
-        [Required(ErrorMessage = "Cannot be blank!")]
+        //[Required(ErrorMessage = "Cannot be blank!")]
         [Display(Name = "Nơi Nhận")]
         public string DiaChiNhan { get; set; }
 
-        [Required(ErrorMessage = "Cannot be blank!")]
+        //[Required(ErrorMessage = "Cannot be blank!")]
         [Display(Name = "SDT")]
         [RegularExpression(@"^(0(32|33|34|35|36|37|38|39|52|56|58|59|70|76|77|78|79|81|82|83|84|85|86|87|88|89|91|94|96|97|98|99))+([0-9]{7})\b", ErrorMessage = "Số điện thoại không hợp lệ.")]
         [StringLength(50)]
         public string SĐT { get; set; }
 
-        [Required(ErrorMessage = "Cannot be blank!")]
+        //[Required(ErrorMessage = "Cannot be blank!")]
         [Display(Name = "Ngày Đặt")]
         public DateTime NgayDatHang { get; set; }
 
-        [Required(ErrorMessage = "Cannot be blank!")]
+        //[Required(ErrorMessage = "Cannot be blank!")]
         [Display(Name = "Ngày Giao")]
         public DateTime NgayGiaoHang { get; set; }
 
@@ -48,7 +48,7 @@ namespace PetStore.Models
         [StringLength(50)]
         public string Note { get; set; }
 
-        [Required(ErrorMessage = "Cannot be blank!")]
+        //[Required(ErrorMessage = "Cannot be blank!")]
         [Display(Name = "Tổng tiền")]
         [Column(TypeName = "money")]
         public decimal? TongTien { get; set; }
